b1152cb98e10998d64df0be29bae14269241922c
cstylianou98 <cstylianou98@gmail.com>
2023-09-11 14:06:42 +0300

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 const fruitForm = document.querySelector("#inputSection form")
-console.log(fruitForm)
+
+const fruitList = document.querySelector("#fruitSection ul" )
+
+const addFruit = (fruit) => {
+    const li = document.createElement("li");
+    li.textContent = fruit;
+    
+    // Add a click event listener to remove the fruit item when clicked
+    li.addEventListener("click", () => {
+        fruitList.removeChild(li);
+    });
+    
+    fruitList.appendChild(li); // basically creates html element and adds the fruit we added and appends to our fruit list
+}
+
+
+
+fruitForm.addEventListener("submit", e => {
+    e.preventDefault()// by default it refreshes page when u submit form, so this command stops it.
+    addFruit(e.target.fruitInput.value) //This should allow us to add the fruit we submit to the html i.e the list
+    e.target.fruitInput.value = ""
+}) 
+
+
+
+
+
+
